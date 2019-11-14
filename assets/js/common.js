8613bd0dfd223fe5441516ce3bc70909f6ee997b
akashlisp <57682020+akashlisp@users.noreply.github.com>
2019-11-14 14:17:56 +0530

    commit

--- assets/js/common.js
+++ assets/js/common.js
@@ -91,6 +91,22 @@ Morris.Bar({
 });
 
 
+Morris.Bar({
+  element: 'morrisBar2',
+  data: [
+    { y: '2006', a: 100, b: 90 },
+    { y: '2007', a: 75,  b: 65 },
+    { y: '2008', a: 50,  b: 40 },
+    { y: '2009', a: 75,  b: 65 },
+    { y: '2010', a: 50,  b: 40 },
+  ],
+  xkey: 'y',
+  ykeys: ['a', 'b'],
+  labels: ['Series A', 'Series B'],
+});
+
+
+
 
 
 
